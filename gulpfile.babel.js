67bfc982fe511a8bd066b130b6e166b56c133bbc
yuichi tanaka <u1tnk1123@gmail.com>
2016-01-05 00:38:03 +0900

    sass/haml/es6で書けるところまで

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,9 @@
 
 import gulp from 'gulp';
 import sass from "gulp-sass";
+import haml from "gulp-haml";
 import autoprefixer from "gulp-autoprefixer";
 import frontnote from "gulp-frontnote";
-import uglify from "gulp-uglify";
 import browser from "browser-sync";
 import plumber from "gulp-plumber";
 import browserify from "browserify";
@@ -19,38 +19,42 @@ gulp.task("sass", () => {
         .pipe(frontnote({css: '../css/style.css'}))
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(gulp.dest("./css"))
+        .pipe(gulp.dest("./publish/css"))
         .pipe(browser.reload({stream:true}));
 });
 
 gulp.task("js", () => {
-    browserify({entries: ["./js.dev/index.js"]})
+    browserify({entries: ["./js/main.js"]})
         .transform(babelify)
         .bundle()
         .on('error', handleErrors)
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest("./js"))
-        .pipe(browser.reload({stream:true}));
+        .pipe(gulp.dest("./publish/js"));
 });
 
-// gulp.task("js", => {
-//     gulp.src(["js.dev/**/*.js"])
-//         .pipe(plumber())
-//         .pipe(uglify())
-//         .pipe(gulp.dest("./js"))
-//         .pipe(browser.reload({stream:true}));
-// });
+gulp.task("haml", () =>{
+    gulp.src("haml/**/*.haml")
+        .pipe(plumber())
+        .pipe(haml({pretty: true}))
+        .pipe(gulp.dest("./publish/"));
+
+});
+
+gulp.task("reload", () =>{
+    browser.reload()
+});
 
 gulp.task("server", () => {
     browser({
         server: {
-            baseDir: "./"
+            baseDir: "./publish/"
         }
     });
 });
 
 gulp.task("default", ['server'], () => {
-    gulp.watch(["js.dev/**/*.js"],["js"]);
-    gulp.watch("sass/**/*.scss",["sass"]);
+    gulp.watch("js/**/*.js", ["js", "reload"]);
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch("haml/**/*.haml", ["haml", "reload"]);
 });
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (() => {
     window.addEventListener('load', () => {
-        console.log('babel loaded!')
+        console.log('babeelllllll loaded!')
    });
 })();
